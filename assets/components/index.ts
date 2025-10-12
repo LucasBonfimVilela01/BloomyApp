5d46de2d36f85a4277a9903f15d0e22eace546ed
Lucas <lucasbvilela01@gmail.com>
2025-10-12 17:20:35 -0300

    Commit-3
    Pagina de informações atualizada.
    Agora sua senha é requisitada ao tentar entrar na página de conta e na página de configurações.

--- assets/components/index.ts
+++ assets/components/index.ts
@@ -4,6 +4,7 @@ export { default as CustomButton } from './CustomButton';
 export { default as InputField } from './InputField';
 export { default as LinhaEspacamento } from './LinhaEspacamento';
 export { default as LinkText } from './LinkText';
+export { default as PasswordConfirmModal } from './PasswordConfirmModal';
 export { default as PasswordField } from './PasswordField';
 export { default as ScreenContainer } from './ScreenContainer';
 export { default as ScreenTitle } from './ScreenTitle';
